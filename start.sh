5fbfcaea375dcacc539028bd514eeba37e9c2ab6
Rplay <americarplay@gmail.com>
2023-07-17 22:02:28 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,15 +1,8 @@
-if [ -z $UPSTREAM_REPO ]
-then
-  echo "Cloning main Repository"
-  git clone https://github.com/JadejaShu/custom-urlshortner /Millie
-else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Millie
-fi
+echo "copying your bot data ...."
+git clone https://github.com/JadejaShu/custom-urlshortner /Millie
 cd /Millie
-
+echo "installing some packages..."
 pip3 install -U -r requirements.txt
-
 echo "Starting Bot...."
 echo  " velentina starting....."
 python3 bot.py
